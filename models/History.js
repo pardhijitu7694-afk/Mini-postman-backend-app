fd02425a6eed3d1719299b1d62d8b4adc1313c35
Jitendra Pardhi <pardhijitu7694@gmail.com>
2025-12-05 14:49:13 +0530

    updated the model

--- models/History.js
+++ models/History.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const HistorySchema = new mongoose.Schema({
   method: { type: String, default: "GET" },
   url: { type: String, default: null },
-  headers: { type: Object, default: null },
-  body: { type: Object, default: null },
+  headers: { type: Object, default: {} },
+  body: { type: Object, default: {} },
   responseStatus: { type: Number, default: null },
   responseTime: { type: Number, default: null },
   createdAt: { type: Date, default: Date.now }
